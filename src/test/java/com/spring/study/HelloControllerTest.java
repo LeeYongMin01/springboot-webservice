626bf945c2149c27dcb0c066e02aaabe8a7aca5d
LeeYongMin01 <vbn0213@gmail.com>
2022-01-21 11:22:48 +0900

    implementation 추가 및 테스트 코드 작성

--- src/test/java/com/spring/study/HelloControllerTest.java
+++ src/test/java/com/spring/study/HelloControllerTest.java
@@ -1,9 +1,29 @@
 package com.spring.study;
 
-public class HelloControllerTest
-{
-	public static void main(String[] args)
-	{
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest
+{
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void helloReturn() throws Exception
+	{
+		String hello = "hello";
+		mockMvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
 	}
 }
