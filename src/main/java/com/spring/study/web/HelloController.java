8b8f8d8d1733d65560b1791a2e52071c197e999c
LeeYongMin01 <vbn0213@gmail.com>
2022-01-22 15:26:50 +0900

    Dto 소스 추가, LomBok 추가

--- src/main/java/com/spring/study/web/HelloController.java
+++ src/main/java/com/spring/study/web/HelloController.java
@@ -1,14 +1,17 @@
 package com.spring.study.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.study.web.dto.HelloResponseDto;
 
 @RestController
 public class HelloController
 {
-	@GetMapping("/hello")
-	public String hello()
+	@GetMapping("/hello/dto")
+	public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount)
 	{
-		return "hello";
+		return new HelloResponseDto(name, amount);
 	}
 }
