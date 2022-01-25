059a57d3e9a9f1b8b9dcce10188f850e145f2203
LeeYongMin01 <vbn0213@gmail.com>
2022-01-25 10:12:44 +0900

    게시물 수정/삭제 기능 구현

--- src/main/java/com/spring/study/web/IndexController.java
+++ src/main/java/com/spring/study/web/IndexController.java
@@ -3,8 +3,10 @@ package com.spring.study.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.spring.study.service.posts.PostsService;
+import com.spring.study.web.dto.PostsResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,5 +27,14 @@ public class IndexController
 	public String postsSave()
 	{
 		return "posts-save";
+	}
+
+	@GetMapping("/posts/update/{id}")
+	public String postsUpdate(@PathVariable Long id, Model model)
+	{
+		PostsResponseDto dto = postsService.findById(id);
+		model.addAttribute("post", dto);
+
+		return "posts-update";
 	}
 }
--- src/main/java/com/spring/study/web/PostsApiController.java
+++ src/main/java/com/spring/study/web/PostsApiController.java
@@ -1,8 +1,10 @@
 package com.spring.study.web;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,10 +28,17 @@ public class PostsApiController
 		return postsService.save(requestDto);
 	}
 
-	@PostMapping("/api/v1/posts/{id}")
+	@PutMapping("/api/v1/posts/{id}")
 	public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto)
 	{
 		return postsService.update(id, requestDto);
+	}
+
+	@DeleteMapping("/api/v1/posts/{id}")
+	public Long delete(@PathVariable Long id)
+	{
+		postsService.delete(id);
+		return id;
 	}
 
 	@GetMapping("/api/v1/posts/{id}")
